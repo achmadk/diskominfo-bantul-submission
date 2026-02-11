b523baaf5e41f7edec49b3a347ca14e5777cedc7
Achmad Kurnianto <achmad_kurnianto@yahoo.co.id>
2026-02-11 11:39:44 +0700

    refactor(resource): :bug: remove unnecessary resources
    delete blog-post and categories from example

--- src/app/blog-posts/layout.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { ThemedLayout } from "@refinedev/mui";
-import { Header } from "@components/header";
-
-export default async function Layout({ children }: React.PropsWithChildren) {
-  return <ThemedLayout Header={Header}>{children}</ThemedLayout>;
-}
